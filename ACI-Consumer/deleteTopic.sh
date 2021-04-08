bafa8cdbe3d8e3da02664ad2915e9c11c98d5afb
soumukhe <soumukhe@cisco.com>
2021-04-07 23:46:37 -0500

    fixed path

--- ACI-Consumer/deleteTopic.sh
+++ ACI-Consumer/deleteTopic.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-~/kafka-docker-compose/kafka_2.13-2.7.0/bin/kafka-topics.sh --bootstrap-server localhost:9092 --delete  --topic test-topic 
+~/kafka-docker-compose-NI/kafka_2.13-2.7.0/bin/kafka-topics.sh --bootstrap-server localhost:9092 --delete  --topic test-topic 
--- ACI-Consumer/startConsumer.sh
+++ ACI-Consumer/startConsumer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# ~/kafka-docker-compose/kafka_2.13-2.7.0/bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic test-topic --from-beginning
-~/kafka-docker-compose/kafka_2.13-2.7.0/bin/kafka-console-consumer.sh --bootstrap-server 10.1.100.13:9092 --topic test-topic
+# ~/kafka-docker-compose-NI/kafka_2.13-2.7.0/bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic test-topic --from-beginning
+~/kafka-docker-compose-NI/kafka_2.13-2.7.0/bin/kafka-console-consumer.sh --bootstrap-server 10.1.100.13:9092 --topic test-topic
