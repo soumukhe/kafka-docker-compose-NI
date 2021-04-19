42455bf5f6542fc7dfb61778e9a15eefb9d00c08
soumukhe <soumukhe@cisco.com>
2021-04-19 14:32:10 -0500

    3rd upload

--- ACI-Consumer/createTopic.sh
+++ ACI-Consumer/createTopic.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
-cd ..
 
-$(pwd)/kafka_2.13-2.7.0/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create  --topic test-topic --replication-factor 1 --partitions 3
+usage() {
+        echo " "
+        echo "Usage:"
+        echo "createTopic.sh some-topic_name"
+        } 
+
+kreateTopic() {
+                cd ..
+		topicname=$1
+		$(pwd)/kafka_2.13-2.7.0/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create  --topic $topicname --replication-factor 1 --partitions 3
+              }
+
+# MAIN
+if [[ $# -eq 0 ]] ; then
+	usage
+    	exit 1
+
+else kreateTopic $1	
+
+fi
+
